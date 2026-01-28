6dc49c8bcd41525ba132d2eb6919775c9de76666
tduncan14 <nbatre1@yahoo.com>
2026-01-28 08:41:35 -0600

    keep moving forward

--- backend/config/ConnectDB.js
+++ backend/config/ConnectDB.js
@@ -1,10 +1,24 @@
 import mongoose from "mongoose"
 
 
-export default connectDB = async () => {
+export const connectDB = async () => {
 
 
-    await mongoose.connect()
+
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+
+        console.log(
+            "database is connected"
+        )
+    }
+
+    catch (err) {
+        console.log(err, 'this is the error')
+        process.exit(1)
+    }
+
 
     // mongodb+srv://treek:<db_password>@cluster0.zee7avz.mongodb.net/?appName=Cluster0
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 // const express = require("express");
 import express from "express";
+import dotenv from "dotenv"
+import { connectDB } from "./config/ConnectDB.js";
+import cors from "cors"
+import path from "path"
 
+dotenv.config()
+
+
+connectDB()
 
 //mongodb + srv://treek:pokemon1@cluster0.zee7avz.mongodb.net/?appName=Cluster0
 
 const app = express();
+
+
+app.use(express.json())
+app.use(cors())
+app.get("/products", async (req, res) => {
+
+    const product = req.body
+
+})
 
 app.get("/", (req, res) => {
 
